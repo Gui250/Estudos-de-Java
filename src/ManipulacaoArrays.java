b06bf0bfcfe982418f4445cc9a08f73d1b5b2ee4
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-11-30 14:38:06 -0300

    alterações feitas

--- src/ManipulacaoArrays.java
+++ src/ManipulacaoArrays.java
@@ -1,5 +1,18 @@
 public class ManipulacaoArrays {
     public static void main(String[] args) {
         int[] array = {1, 234, 987};
+//        int[] array2 = {1, 234, 987};
+//        int array3[] = {1, 234, 987};
+//        System.out.println(array[0]);
+
+        int[] array4 = new int[5];
+        array4[0] = 2000;
+        array4[1] = 2001;
+        array4[2] = 2002;
+        array4[3] = 2003;
+        array4[4] = 2004;
+        array4[5] = 2005;
+
+        System.out.println(array4[0]);
     }
 }
