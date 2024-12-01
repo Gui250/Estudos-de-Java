f4cb38040d7e03ae21169c2bb722ce9d1bff9d44
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-12-01 11:17:12 -0300

    Arquivo de loops criado

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+        int[] arraySimples = {1, 2, 3, 4};
+
+      //  System.out.println(arraySimples.length);
+
+        for(int i = 0; i <= arraySimples.length; i++) {
+            System.out.println(arraySimples[i]);
+        }
+    }
+}
