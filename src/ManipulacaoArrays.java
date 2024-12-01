e84bf9dd1c7c8048fe8c29caac16a3c4fb228382
Guilherme Moreno <guilhermemoreno200@gmail.com>
2024-12-01 09:45:39 -0300

    adicionei arraysMuldimensionais

--- src/ManipulacaoArrays.java
+++ src/ManipulacaoArrays.java
@@ -5,14 +5,24 @@ public class ManipulacaoArrays {
 //        int array3[] = {1, 234, 987};
 //        System.out.println(array[0]);
 
-        int[] array4 = new int[5];
-        array4[0] = 2000;
-        array4[1] = 2001;
-        array4[2] = 2002;
-        array4[3] = 2003;
-        array4[4] = 2004;
-        array4[5] = 2005;
+//        int[] array4 = new int[5];
+//        array4[0] = 2000;
+//        array4[1] = 2001;
+//        array4[2] = 2002;
+//        array4[3] = 2003;
+//        array4[4] = 2004;
+//        array4[5] = 2005;
+//
+//        System.out.println(array4[0]);
 
-        System.out.println(array4[0]);
+        // Listas Multidimensionais
+//        String[][] arrayMultidimensional = {{"Boby", "João"}, {"Maria", "Antonia"}};
+//        System.out.println(arrayMultidimensional[0][1]);
+
+        int[][] arrayMultidimensional2 =  new int[2][2];
+        arrayMultidimensional2[0][0] = 1;
+        arrayMultidimensional2[1][0] = 2;
+        arrayMultidimensional2[1][0] = 3;
+        System.out.println(arrayMultidimensional2[0][1]);
     }
 }
